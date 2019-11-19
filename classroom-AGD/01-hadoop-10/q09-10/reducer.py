2f79d078e651bef942c51fd18078cfe24315e0bb
jdvelasq <jdvelasq@unal.edu.co>
2019-11-19 15:17:08 -0500

    readme.md 01-hadoop--q09

--- classroom-AGD/01-hadoop-10/q09-10/reducer.py
+++ classroom-AGD/01-hadoop-10/q09-10/reducer.py
@@ -1,10 +1,3 @@
-#
-# Pregunta
-# ===========================================================================
-#
-# Escriba un job de hadoop (en Python) que obtenga los 5 registros con 
-# valores más pequeños en la tercera columna del  archivo `data.csv`.  
-#
 import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
