0220c589283531689f2d98fef92b6c59770a9f2d
jdvelasq <jdvelasq@unal.edu.co>
2019-11-19 15:16:10 -0500

    readme.md 01-hadoop-q08

--- classroom-AGD/01-hadoop-10/q07-10/mapper.py
+++ classroom-AGD/01-hadoop-10/q07-10/mapper.py
@@ -1,10 +1,3 @@
-#
-# Pregunta
-# ===========================================================================
-#
-# Escriba un job de hadoop (en Python) que ordene el archivo `data.csv`  por 
-# letra y valor (3ra columna).
-#
 import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
