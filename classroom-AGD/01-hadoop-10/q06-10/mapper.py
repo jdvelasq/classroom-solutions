77b5774ae94a4040d8aadf6a397576c7856fd073
jdvelasq <jdvelasq@unal.edu.co>
2019-11-19 15:13:13 -0500

    readme.md 01-hadoop-q06

--- classroom-AGD/01-hadoop-10/q06-10/mapper.py
+++ classroom-AGD/01-hadoop-10/q06-10/mapper.py
@@ -1,10 +1,3 @@
-#
-# Pregunta
-# ===========================================================================
-#
-# Escriba un job de hadoop (en Python) que compute los valores máximo y 
-# mínimo de la tercera columna por letra, para el archivo `data.csv`.
-#
 import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
--- classroom-AGD/01-hadoop-10/q06-10/reducer.py
+++ classroom-AGD/01-hadoop-10/q06-10/reducer.py
@@ -1,13 +1,6 @@
-#
-# Pregunta
-# ===========================================================================
-#
-# Escriba un job de hadoop (en Python) que compute los valores máximo y 
-# mínimo de la tercera columna por letra, para el archivo `data.csv`.
-#
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 curkey = None
 total = 0
