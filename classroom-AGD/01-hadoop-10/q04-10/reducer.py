94ef91768c303b76e5a9e611b034bbccbcd91897
jdvelasq <jdvelasq@unal.edu.co>
2019-11-19 15:10:02 -0500

    readme.md 01-hadoop-q04

--- classroom-AGD/01-hadoop-10/q04-10/reducer.py
+++ classroom-AGD/01-hadoop-10/q04-10/reducer.py
@@ -1,13 +1,6 @@
-#
-#  Pregunta
-#  ===========================================================================
-#
-#  Escriba un job de hadoop (en Python) que ordene el archivo `data.csv` por
-#  la segunda columna, de menor a mayor.
-#
 import sys
 #
-#  >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 curkey = None
 total = 0
