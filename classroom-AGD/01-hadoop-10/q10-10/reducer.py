413da992d325b68391805de0e85969b4b6363624
jdvelasq <jdvelasq@unal.edu.co>
2019-11-19 15:18:32 -0500

    readme.md 01-hadoop--q10

--- classroom-AGD/01-hadoop-10/q10-10/reducer.py
+++ classroom-AGD/01-hadoop-10/q10-10/reducer.py
@@ -1,13 +1,6 @@
-#
-# Pregunta
-# ===========================================================================
-#
-# Escriba un job de hadoop (en Python) que obtenga las letras asociadas 
-# (columna 2) a cada clave (columna 1) del  archivo `data.csv`.  
-#
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 curkey = None
 docs = []
