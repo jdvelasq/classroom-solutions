f94acb44f701965c1bcd93189d5a393d9291abb0
jdvelasq <jdvelasq@unal.edu.co>
2019-12-19 17:36:50 -0500

    Update mapper.py

--- classroom-AGD/01-hadoop-50/q07-10/mapper.py
+++ classroom-AGD/01-hadoop-50/q07-10/mapper.py
@@ -4,6 +4,7 @@ import sys
 #
 for line in sys.stdin:
     line = line.replace('\n', '')
+    line = line.replace('\t', '')
     row = line.split(' ')
     row = [x for x in row if x != '']
     key = row[0] + row[2].rjust(5, '0')
