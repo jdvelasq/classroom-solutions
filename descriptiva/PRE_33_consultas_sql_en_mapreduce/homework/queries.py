8e508a714a52e9555909d180a65bdcbeea3cdeec
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-09-18 23:19:28 -0500

    jdvelasq/PRE_consultas_sql_en_mapreduce

---
+++ descriptiva/PRE_33_consultas_sql_en_mapreduce/homework/queries.py
@@ -0,0 +1,16 @@
+"""Taller evaluable"""
+
+# pylint: disable=broad-exception-raised
+# pylint: disable=import-error
+
+
+#
+# ORQUESTADOR:
+#
+def run():
+    """Orquestador"""
+
+
+if __name__ == "__main__":
+
+    run()
