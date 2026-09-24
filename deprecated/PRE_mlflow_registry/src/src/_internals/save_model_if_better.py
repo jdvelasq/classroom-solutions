a6f4f151258c7be94f35aca89ebc5d3f9adc96c6
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-09-24 12:14:49 -0500

    update

---
+++ deprecated/PRE_mlflow_registry/src/src/_internals/save_model_if_better.py
@@ -0,0 +1,15 @@
+import os
+import pickle
+
+from deprecated.PRE_mlflow_registry.src.src._internals.compare_models import compare_models
+from deprecated.PRE_mlflow_registry.src.src._internals.save_model import save_model
+
+
+def save_model_if_better(model, x_test, y_test, save_path="models/estimator.pkl"):
+    """Save the model if it performs better than the existing one."""
+    best_model = None
+    if os.path.exists(save_path):
+        with open(save_path, "rb") as file:
+            best_model = pickle.load(file)
+    best_model = compare_models(model, best_model, x_test, y_test)
+    save_model(best_model, save_path)
